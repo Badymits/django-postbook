2ed71c399fa999cbbcf751d5e627b758327aee40
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-11 22:37:36 +0800

    created login and register view

--- postbook/accounts/views.py
+++ postbook/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from .models import Account
+from .forms import RegisterModelForm, LoginModelForm
+
 # Create your views here.
+def loginView(request):
+    form = LoginModelForm()
+    context = {'login_form': form}
+    
+    return render(request, 'accounts/login.html', context)
+
+def registerView(request):
+    form = RegisterModelForm()
+    context = {'reg_form': form}
+    return render(request, 'accounts/register.html', context)
