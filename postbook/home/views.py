5291019c081506fb3c898608a726940c23f11882
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-11 22:38:27 +0800

    added login req decorator. added context variable

--- postbook/home/views.py
+++ postbook/home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
     
     return HttpResponse('Hello World!!')
 
 # all funcs will be camel cases
+@login_required(login_url='login')
 def homePage(request):
-
-    return render(request, 'home/home.html')
+    context = {'user': request.user}
+    return render(request, 'home/home.html', context)
