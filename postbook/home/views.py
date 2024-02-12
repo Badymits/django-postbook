eaa4163b505da21114cda6bb0cf35673fe471bf9
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-12 17:27:12 +0800

    created createpostview and added logic to home and createpost views

--- postbook/home/views.py
+++ postbook/home/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib import messages 
 from django.contrib.auth.decorators import login_required
+
+from accounts.models import Account
+from .models import Post
+from .forms import CreatePostForm
 
 # Create your views here.
 def index(request):
@@ -11,5 +15,32 @@ def index(request):
 # all funcs will be camel cases
 @login_required(login_url='login')
 def homePage(request):
-    context = {'user': request.user}
+    
+    posts = Post.objects.all()
+    context = {'user': request.user, 'posts': posts}
     return render(request, 'home/home.html', context)
+
+@login_required(login_url='login')
+def createPost(request):
+    
+    form = CreatePostForm()
+    req_user = Account.objects.get(email=request.user.email)
+    
+    if request.method == 'POST':
+        form = CreatePostForm(request.POST)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            
+            post.user = req_user
+            form.save()
+            
+            messages.success(request, 'POST MADE!')
+            return redirect('home')
+        else:
+            print('ERROR')
+            messages.error(request, 'Something went wrong...')
+            
+    context = {'form': form}
+    
+    return render(request, 'home/create_post.html', context)
