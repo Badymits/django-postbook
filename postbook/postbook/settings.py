eea811c8a99d2ba0690dbcc56c357b50e2073edf
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-12 17:28:55 +0800

    configured media url and static file dirs

--- postbook/postbook/settings.py
+++ postbook/postbook/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
