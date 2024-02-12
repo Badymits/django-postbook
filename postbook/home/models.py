e25a57177df26be9e2b4ff2928aa5cb70541a7c4
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-12 17:26:36 +0800

    created post model

--- postbook/home/models.py
+++ postbook/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
 
 # Create your models here.
+class Post(models.Model):
+    
+    user                = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title               = models.CharField(max_length=255)
+    body                = models.CharField(max_length=355)
+    date_posted         = models.DateTimeField(auto_now_add=True)
+    image               = models.ImageField(blank=True, null=True)
+    slug                = models.SlugField(default="", max_length=255, blank=True, null=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
