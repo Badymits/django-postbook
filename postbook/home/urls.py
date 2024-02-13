7f2f680955daba47a15bc0280f779d4a7afa23ce
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-13 23:04:35 +0800

    added url path for create comment view

--- postbook/home/urls.py
+++ postbook/home/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.homePage, name='home'),
     path('create-post/', views.createPost, name='create-post'),
     path('detail-post/<int:id>/', views.detailPost, name='detail-post'),
+    
+    path('create-comment/<int:id>/', views.createComment, name='create-comment'),
 ]
