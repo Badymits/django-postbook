d16ef05cd8a95dd29d5ae96afd993ec143f46bcd
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-13 23:03:33 +0800

    created comment form model class

--- postbook/home/forms.py
+++ postbook/home/forms.py
@@ -1,10 +1,17 @@
 from django import forms
 
-from .models import Post
+from .models import Post, Comment
 
 class CreatePostForm(forms.ModelForm):
     
     class Meta:
         model = Post
         fields = ['title', 'body', 'image']
+        
+        
+class CreateCommentForm(forms.ModelForm):
+    
+    class Meta:
+        model = Comment
+        fields = ['body', 'image']
 
