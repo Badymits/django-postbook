04013e4a467dcdb9108d8c0a82f1fa140e05df3f
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-15 01:11:03 +0800

    added url path for edit post and edit comment views

--- postbook/home/urls.py
+++ postbook/home/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.homePage, name='home'),
     path('create-post/', views.createPost, name='create-post'),
     path('detail-post/<int:id>/', views.detailPost, name='detail-post'),
+    path('edit-post/<int:id>/', views.editPost, name='edit-post'),
     
     path('create-comment/<int:id>/', views.createComment, name='create-comment'),
+    path('edit-comment/<int:id>/', views.editComment, name='edit-comment'),
 ]
