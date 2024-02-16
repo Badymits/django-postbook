e6e715e5fe72f5774746b4a69dc43d8efc6a9f24
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-16 16:35:08 +0800

    cretaed url path for delete comment and delete post views.

--- postbook/home/urls.py
+++ postbook/home/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('create-post/', views.createPost, name='create-post'),
     path('detail-post/<int:id>/', views.detailPost, name='detail-post'),
     path('edit-post/<int:id>/', views.editPost, name='edit-post'),
+    path('delete-post/<int:id>/', views.deletePost, name='delete-post'),
     
     path('create-comment/<int:id>/', views.createComment, name='create-comment'),
     path('edit-comment/<int:id>/', views.editComment, name='edit-comment'),
+    path('delete-comment/<int:id>/', views.deleteComment, name='delete-comment'),
 ]
