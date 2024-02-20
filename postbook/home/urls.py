88488459f3f2c5184c9c494617c1dd02977d02ec
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-21 00:33:29 +0800

    created url path for like and dislike post and comments

--- postbook/home/urls.py
+++ postbook/home/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
     path('detail-post/<int:id>/', views.detailPost, name='detail-post'),
     path('edit-post/<int:id>/', views.editPost, name='edit-post'),
     path('delete-post/<int:id>/', views.deletePost, name='delete-post'),
+    path('like-post/<int:id>/', views.likePost, name='like-post'),
+    path('dislike-post/<int:id>/', views.dislikePost, name='dislike-post'),
     
     path('create-comment/<int:id>/', views.createComment, name='create-comment'),
     path('edit-comment/<int:id>/', views.editComment, name='edit-comment'),
     path('delete-comment/<int:id>/', views.deleteComment, name='delete-comment'),
+    path('like-comment/<int:id>/', views.likeComment, name='like-comment'),
+    path('dislike-comment/<int:id>/', views.dislikeComment, name='dislike-comment'),
 ]
