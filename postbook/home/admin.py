1ce8fae5e07f1e35d03501c1eaca1ec9e5b8b9da
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-21 00:31:50 +0800

    registered like and dislike model

--- postbook/home/admin.py
+++ postbook/home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, Comment
+from .models import Post, Comment, LikeModel, DislikeModel
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(LikeModel)
+admin.site.register(DislikeModel)
