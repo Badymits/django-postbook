c9ffd8bb2d933094fd7484e04c62228eaba6a8a6
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-02-21 23:39:16 +0800

    created updatevote view to handle user like or dislike on posts.

--- postbook/home/views.py
+++ postbook/home/views.py
@@ -134,48 +134,58 @@ def deletePost(request, id):
         return JsonResponse(context)
     
 @login_required(login_url='login')
-def likePost(request, id):
+def updateVotePost(request, id):
     
     context = {}
     
     try:
         post = get_object_or_404(Post, id=id)
-        print('THE POST', post)
-        
-        try:
-            like_obj = get_object_or_404(LikeModel, id=post.id)
-        except:
-            
-            like_obj = LikeModel.objects.create(
-                post=post,
-            )
-            print('CREATED!!!!')
-            context['message'] = 'Liked Post!'
-        print('WHAT IFIFIFIFIF: ', like_obj.users.all())
-            
-        if request.user in like_obj.users.exists():
-            print('TANGGAL!!!')
-            like_obj.users.remove(request.user)
-            print('NAG RUN DITO TANGINA!!!!')
-        else:
-            print('DAGDAG!!!!!!')
-            like_obj.users.add(request.user)
-            
-            print('NAG RUN DITO TANGINA')
-            
-        
-        return JsonResponse(context)
-        
+
     except:
         
         context['message'] = 'Post does not exist'
         return JsonResponse(context)
-
-
-@login_required(login_url='login')
-def dislikePost(request, id):
     
-    pass
+    
+    if request.GET.get('option') == 'like':
+        
+        try:
+            like_obj = get_object_or_404(LikeModel, post=post)
+        except:
+            like_obj = LikeModel.objects.create(
+                post=post,
+            )
+        context['message'] = 'Liked Post!'
+        
+        if like_obj.users.filter(id=request.user.id).exists():
+            like_obj.users.remove(request.user)
+            context['option'] = 'removed like'
+        else:
+            like_obj.users.add(request.user)
+            context['option'] = 'added'
+        context['like_count'] = like_obj.users.all().count()
+            
+    elif request.GET.get('option') == 'dislike':
+        
+        try:
+            dislike_obj = get_object_or_404(DislikeModel, post=post)
+        except:
+            dislike_obj = DislikeModel.objects.create(
+                post=post
+            )
+        context['message'] = 'Disliked Post!'
+        
+        if dislike_obj.users.filter(id=request.user.id).exists():
+            dislike_obj.users.remove(request.user)
+            context['option'] = 'removed dislike'
+        else:
+            dislike_obj.users.add(request.user)
+            context['option'] = 'added'
+            
+        context['like_count'] = dislike_obj.users.all().count()
+    
+    return JsonResponse(context)
+
 
 
 @login_required(login_url='login')
