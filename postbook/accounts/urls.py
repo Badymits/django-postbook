e01b39f5a2267860724b057a742cca6cf0855b9a
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-03-01 22:16:47 +0800

    created url path for edit profile view

--- postbook/accounts/urls.py
+++ postbook/accounts/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('register/', views.registerView, name='register'),
     path('logout/', views.logoutView, name='logout'),
     path('user/<int:id>/<str:tab>/', views.userView, name='user'),
-    path('settings/<str:tab>/', views.settingsView, name='settings')
+    path('settings/<str:tab>/', views.settingsView, name='settings'),
+    
+    path('edit-profile/<int:id>/', views.editProfile, name='edit-profile'),
 ]
