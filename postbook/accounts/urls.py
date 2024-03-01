3f1822f6d7481835ab4d541cb81ac280c79c55d7
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-03-01 18:41:42 +0800

    added url path for settings view

--- postbook/accounts/urls.py
+++ postbook/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register/', views.registerView, name='register'),
     path('logout/', views.logoutView, name='logout'),
     path('user/<int:id>/<str:tab>/', views.userView, name='user'),
+    path('settings/<str:tab>/', views.settingsView, name='settings')
 ]
